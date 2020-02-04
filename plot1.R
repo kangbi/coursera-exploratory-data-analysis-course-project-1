1d7d7e14e7d791a7f6e190b6c43dc1ac1d987e0f
kangbi <hyukjoon.kim@gmail.com>
2020-02-04 20:02:21 +0900

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+
+data   <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+mydata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+GAP <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(GAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
